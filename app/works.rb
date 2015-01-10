c5f9dc17b985d701f1b707c5f110fcf74eaa8990
Dave Looi <david.looi@mindvalley.com>
2015-01-10 10:39:55 +0800

    Uniqfy bug

--- app/works.rb
+++ app/works.rb
@@ -1,21 +1,20 @@
 class Works
   attr_reader :works, :uniq_make, :uniq_model
 
-  def initialize(file)
-    @file = file
+  def initialize(xml) # xml could be xml string or xml file handle
+    @xml = xml
     parse_xml
   end
 
 private
   def parse_xml
-    @doc = Nokogiri::XML(@file) do |config|
+    @doc = Nokogiri::XML(@xml) do |config|
       config.noblanks
     end
 
     @works = []
     all_make = []
     all_model = []
-
     @doc.child.children.each do |child_node|
       work = Work.new(child_node)
       @works << work
@@ -27,7 +26,7 @@ private
   end
 
   def uniqfy_make_model(all_make, all_model)
-    @uniq_make = all_make.uniq!
-    @uniq_model = all_model.uniq!
+    @uniq_make = all_make.uniq
+    @uniq_model = all_model.uniq
   end
 end
